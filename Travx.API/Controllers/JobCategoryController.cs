9abdeeea148eb37947a52674a447ece45a5fee6d
Naxo166 <sallahmad059@gmail.com>
2022-03-24 14:39:37 +0200

    Job Category Controller

---
+++ Travx.API/Controllers/JobCategoryController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Travx.API.Controllers;
+
+[Route("api/[Controller]")]
+[ApiController]
+public class JobCategoryController : ControllerBase
+{
+    public JobCategoryController()
+    {
+    }
+}
