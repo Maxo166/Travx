8a8924b7e610d2e6eac2cde7909502aaa089e8a0
Naxo166 <sallahmad059@gmail.com>
2022-03-27 16:35:33 +0200

    travx-client with react

---
+++ travx-client/src/components/JobLists.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class JobLists extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>Job Lists</div>
+    )
+  }
+}
+
+export default JobLists
